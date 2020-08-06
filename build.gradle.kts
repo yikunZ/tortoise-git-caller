c8541bbdd6ad17a8e72830070ed9fcb7f43c23e6
yikunZ <784427059@qq.com>
2020-08-06 23:09:54 +0800

    修改支持的版本信息。

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,8 +88,8 @@ tasks {
 
     patchPluginXml {
         version(pluginVersion)
-        sinceBuild(pluginSinceBuild)
-        untilBuild(pluginUntilBuild)
+        // sinceBuild(pluginSinceBuild)
+        // untilBuild(pluginUntilBuild)
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         pluginDescription(closure {
